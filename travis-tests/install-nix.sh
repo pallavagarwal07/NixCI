381256fed7f027e822a7ffa67d1dcffa27e4e6de
Pallav Agarwal <pallavagarwal07@gmail.com>
2017-12-06 01:42:43 +0530

    Add .travis.yml file for CI.

--- travis-tests/install-nix.sh
+++ travis-tests/install-nix.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# Assume originally running as root
 sudo apt-get update
 sudo apt-get -y install bzip2 sudo curl bash
 
-cd "$HOME"
+cd "${HOME}"
 bash <(curl https://nixos.org/nix/install)
-mkdir -p $HOME/.nixpkgs
+mkdir -p ${HOME}/.nixpkgs
 echo '{
   nix.binaryCaches = ["http://hydra.nixos.org/" "http://cache.nixos.org/"];
-}' > $HOME/.nixpkgs/config.nix
+}' > ${HOME}/.nixpkgs/config.nix
