5f21ee12f08c1a1c339f31e910b9c1f3afb8514a
Pallav Agarwal <pallavagarwal07@gmail.com>
2020-07-14 02:29:48 +0530

    Update #2.

--- travis-tests/install-nix.sh
+++ travis-tests/install-nix.sh
@@ -7,6 +7,8 @@ cd "${HOME}"
 set +x
 bash <(curl https://nixos.org/nix/install)
 echo "Installation done!"
+nix --version
+tree "${HOME}"/.n*
 mkdir -p ${HOME}/.nixpkgs
 echo '{
   nix.binaryCaches = ["http://hydra.nixos.org/" "http://cache.nixos.org/"];
