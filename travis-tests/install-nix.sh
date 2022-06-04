91ea5ac1e0ed55bdf1e11c1a343676a83c56709c
Pallav Agarwal <pallavagarwal07@gmail.com>
2022-06-05 03:32:19 +0530

    Update install-nix.sh

--- travis-tests/install-nix.sh
+++ travis-tests/install-nix.sh
@@ -7,6 +7,7 @@ cd "${HOME}"
 set +x
 sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
 curl -L https://nixos.org/nix/install | sh
+. /home/user/.nix-profile/etc/profile.d/nix.sh
 echo "Installation done!"
 nix --version
 tree "${HOME}"/.n*
